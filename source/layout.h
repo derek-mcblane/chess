51e0c5b252e8134e77716637fe77c048658cb8f6
Derek McBlane <mcblanederek@gmail.com>
2023-07-28 09:28:26 -0400

    remove unused scratch source

--- source/layout.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "sdlpp.h"
-
-template <typename Rectangle>
-inline Rectangle remove_margin(Rectangle original, sdl::rectangle_dimension_type<Rectangle> padding)
-{
-    return {original.x - padding, original.y - padding, original.w - 2 * padding, original.h - 2 * padding};
-}
